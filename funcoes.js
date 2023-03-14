447306d9be077d406bed58c7d10fe6da92904ba1
Hícaro Teixeira <127115438+hicaroteixeira@users.noreply.github.com>
2023-03-14 10:35:25 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,12 @@
+function aplicarEfeitos(corFundo, caminhoimagem){
+    alert(corFundo);
+    document.bgColor=corFundo; 
+  caminhoImagem = caminhoImagem.replace("C:\\fakepath\\","");
+    document.images [0].src=caminhoimagem;
+    
+    
+    
+   
+
+
+}
